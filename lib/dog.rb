58d75c92d1a57b6a449c1bec0a6a072fd6796dcc
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-01-16 02:23:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,20 +9,33 @@ class Dog
   def initialize(name)
     @name = name
     # @all = all
+<<<<<<< HEAD
     # @@all << self
     self.save
+=======
+    @@all << self
+>>>>>>> a725b6a0f97aff5eeab3ce1567ca48521b44d864
   end
 
   def self.all
     @@all
   end
 
+<<<<<<< HEAD
 # binding.pry
 
   def self.print_all
 
     @@all.each do |dogs|
       puts dogs.name
+=======
+binding.pry
+
+  def self.print_all
+
+    @@all[name].each do |dogs|
+      puts dogs
+>>>>>>> a725b6a0f97aff5eeab3ce1567ca48521b44d864
     end
   end
 
@@ -30,8 +43,11 @@ class Dog
     @@all.clear
   end
 
+<<<<<<< HEAD
   def save
     @@all << self
   end
 
+=======
+>>>>>>> a725b6a0f97aff5eeab3ce1567ca48521b44d864
 end
